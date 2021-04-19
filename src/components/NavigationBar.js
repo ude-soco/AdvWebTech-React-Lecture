e7dc0b957d0e24b6fef227315a57d32580d110ac
Shoeb Joarder <shoebjoarder@outlook.com>
2021-04-19 09:10:43 +0200

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,38 +1,37 @@
-import React, { Component } from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Button, Nav, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 class NavigationBar extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
-    let gutter = {
-      margin: "0px 8px 0px 8px",
-    };
-    let linkColor = {
-      color: "#fff",
-    };
+  	// styling
+    let gutter = {margin: "0px 8px 0px 8px"};
+    let linkColor = {color: "#fff"};
+
     return (
       <Navbar bg="warning" variant="light" expand="lg">
         <Navbar.Brand href="/">Lecture: React Router</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Button className="btn-info" style={gutter}>
+            <Button variant="info" style={gutter}>
               <Link to="/" style={linkColor}>
                 First counter
               </Link>
             </Button>
 
-            <Button variant="primary" className="btn-primary" style={gutter}>
+            <Button variant="primary" style={gutter}>
               <Link to="/second" style={linkColor}>
                 Second Counter
               </Link>
             </Button>
 
-            <Button variant="success" className="btn-primary" style={gutter}>
+            <Button variant="success" style={gutter}>
               <Link to="/third" style={linkColor}>
                 Third Counter
               </Link>
